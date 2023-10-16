46e8c37629d83fcab60985bf04a7a53e693fe5ec
crqst <145231886+crqst@users.noreply.github.com>
2023-10-16 13:31:47 -0700

    b64 encoder decoder

--- src/crackme.c
+++ src/crackme.c
@@ -6,9 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <b64/cencode.h>
+#include <b64/cdecode.h>
 #define MAXINPUT 32
 
-void base64_encode_wrapper(const char* input, char* output) {
+void 
+base64_encode_wrapper(const char* input, char* output) 
+{
     base64_encodestate state;
     base64_init_encodestate(&state);
     int length = strlen(input);
@@ -25,13 +28,24 @@ void base64_encode_wrapper(const char* input, char* output) {
 
 
 
+void 
+base64_decode_wrapper(const char* input, char* output)
+{
+base64_decodestate state;
+base64_init_decodestate(&state);
+int length = strlen(input);
+int decoded_length = base64_decode_block(input, length, output, &state);
+decoded_length= base64_decode_block(input, length, output, &state);
+output[decoded_length] = '\0';
+}
+
 
 int 
 main(void)
 {
 	//Getline includes the newline character so this is a hacky workaround for now
 	//Ideally this password will be stored in something other than plaintext
-	char* not_the_password = "CantHackMyLayerEight\n";
+	char* not_the_password = "Q2FudEhhY2tNeUxheWVyRWlnaHQK";
 	char* buffer;
 	size_t bufsize = MAXINPUT;
 	buffer = malloc(bufsize * sizeof(char));
@@ -40,9 +54,14 @@ main(void)
 	
 	getline(&buffer, &bufsize, stdin);
 	char encoded_input[MAXINPUT * 2];  // Adjust the size accordingly
+	char decoded_output[MAXINPUT * 2];
     	base64_encode_wrapper(buffer, encoded_input);
+
+	base64_decode_wrapper(not_the_password, decoded_output);
+
 	printf("%s\n", encoded_input);
-	if (!strncmp(buffer, not_the_password, MAXINPUT))
+	printf("%s\n", decoded_output);
+	if (!strncmp(encoded_input, decoded_output, MAXINPUT))
 	{
 		printf("Correct Password!\n");
 		exit(EXIT_SUCCESS);
